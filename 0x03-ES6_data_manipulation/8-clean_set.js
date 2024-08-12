3235c5bd052d5b88a257549c31756d0bdd850583
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-08-12 19:15:40 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString === '') {
     return '';
   }
   const result = [];
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   }
