850ebb74f6ba812a00f1e7dfce49aa5739aa4297
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-10-01 15:04:39 +0300

    Debugging

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,70 +1,75 @@
 const http = require('http');
 const { readFile } = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const hostAddress = '127.0.0.1';
+const hostPort = 1245;
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+function parseStudentData(filePath) {
+  const studentData = {};
+  const courseCounts = {};
+  let totalStudents = 0;
+
   return new Promise((resolve, reject) => {
-    readFile(fileName, (err, data) => {
+    readFile(filePath, (err, fileContent) => {
       if (err) {
         reject(err);
       } else {
-        let output = '';
-        const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
+        let resultOutput = '';
+        const fileLines = fileContent.toString().split('\n');
+        fileLines.forEach((line) => {
+          if (line) {
+            totalStudents += 1;
+            const studentDetails = line.toString().split(',');
+            const course = studentDetails[3];
+
+            if (studentData[course]) {
+              studentData[course].push(studentDetails[0]);
             } else {
-              students[field[3]] = [field[0]];
+              studentData[course] = [studentDetails[0]];
             }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
+
+            if (courseCounts[course]) {
+              courseCounts[course] += 1;
             } else {
-              fields[field[3]] = 1;
+              courseCounts[course] = 1;
             }
           }
-        }
-        const l = length - 1;
-        output += `Number of students: ${l}\n`;
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            output += `Number of students in ${key}: ${value}. `;
-            output += `List: ${students[key].join(', ')}\n`;
+        });
+
+        const studentCount = totalStudents - 1;
+        resultOutput += `Number of students: ${studentCount}\n`;
+        Object.entries(courseCounts).forEach(([course, count]) => {
+          if (course !== 'field') {
+            resultOutput += `Number of students in ${course}: ${count}. `;
+            resultOutput += `List: ${studentData[course].join(', ')}\n`;
           }
-        }
-        resolve(output);
+        });
+
+        resolve(resultOutput);
       }
     });
   });
 }
 
-const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  if (request.url === '/') {
-    response.write('Hello Holberton School!');
-    response.end();
-  }
-  if (request.url === '/students') {
-    response.write('This is the list of our students\n');
-    countStudents(process.argv[2].toString()).then((output) => {
-      const outString = output.slice(0, -1);
-      response.end(outString);
+const serverApp = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    parseStudentData(process.argv[2].toString()).then((studentOutput) => {
+      const trimmedOutput = studentOutput.slice(0, -1);
+      res.end(trimmedOutput);
     }).catch(() => {
-      response.statusCode = 404;
-      response.end('Cannot load the database');
+      res.statusCode = 404;
+      res.end('Cannot load the database');
     });
   }
 });
 
-app.listen(port, hostname, () => {
-});
+serverApp.listen(hostPort, hostAddress, () => {});
 
-module.exports = app;
+module.exports = serverApp;
