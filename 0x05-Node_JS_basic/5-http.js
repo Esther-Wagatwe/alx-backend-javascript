dfe27bf81eb1eeff86884a51f3f8a0a5b0505247
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-10-01 14:46:39 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,47 +1,29 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const serverHost = '127.0.0.1';
+const serverPort = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const database = process.argv[2];
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n');
-    if (database) {
-      const filePath = path.resolve(database);
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          res.end(`Cannot load the database: ${err.message}`);
-        } else {
-          const lines = data.split('\n').filter((line) => line.trim() !== '');
-          const students = lines.slice(1).map((line) => line.split(',')[0]);
-          const fields = lines.slice(1).map((line) => line.split(',')[3]);
-          const fieldCounts = fields.reduce((acc, field) => {
-            if (field) {
-              acc[field] = (acc[field] || 0) + 1;
-            }
-            return acc;
-          }, {});
-          let response = `Number of students: ${students.length}\n`;
-          for (const [field, count] of Object.entries(fieldCounts)) {
-            response += `Number of students in ${field}: ${count}. List: ${students.filter((_, idx) => fields[idx] === field).join(', ')}\n`;
-          }
-          res.end(response.trim());
-        }
+    let studentInfo = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        studentInfo += msg;
+        res.end(studentInfo);
+      })
+      .catch((err) => {
+        studentInfo += err.message;
+        res.end(studentInfo);
       });
-    } else {
-      res.end('Cannot load the database');
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
   }
 });
 
-app.listen(1245);
+app.listen(serverPort, serverHost, () => {
+  console.log(`Server running at http://${serverHost}:${serverPort}`);
+});
 
-module.exports = app;
+module.exserverPorts = app;
